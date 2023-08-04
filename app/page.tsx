6728603d7be5f8d87a6e010b8fce83d18926e24e
bibhorsharma7 <bibhorsharma7@gmail.com>
2023-08-04 15:25:18 -0400

    update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import Button from "@/components/button";
 import Dropzone from "@/components/dropzone";
+import Table from "@/components/table";
+import { useState } from "react";
 
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-normal p-24">
       <div className="flex w-full flex-col items-center space-y-4">
-        <Dropzone />
-        <Button />
+        <Dropzone file={file} setFile={setFile} />
+        <Table file={file} />
+        <Button file={file} />
       </div>
     </main>
   );
