254869c72f47db1d35f94287f7a79efa09d663b9
bibhorsharma7 <bibhorsharma7@gmail.com>
2023-08-04 15:25:03 -0400

    add file drag/drop functionality

--- components/dropzone.tsx
+++ components/dropzone.tsx
@@ -1,14 +1,34 @@
-"use client";
-
 import Image from "next/image";
-import { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 
-const Dropzone = () => {
-  const [file, setFile] = useState(null);
-  // const [file, setFile] = useState("test.jpg");
+const Dropzone = ({
+  file,
+  setFile,
+}: {
+  file: File | null;
+  setFile: Dispatch<SetStateAction<File | null>>;
+}) => {
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+
+    let f = null;
+    if (e.dataTransfer && e.dataTransfer.files[0]) {
+      f = e.dataTransfer.files[0];
+    }
+    setFile(f);
+  };
 
   return (
-    <div className="flex h-52 w-full flex-col items-center rounded-lg border-2 border-dashed border-gray-500 p-4">
+    <div
+      className="flex h-52 w-full flex-col items-center rounded-lg border-2 border-dashed border-gray-500 p-4"
+      onDrop={(e) => handleDrop(e)}
+      onDragOver={(e) => handleDragOver(e)}
+      onClick={(e) => e.preventDefault()}
+    >
       <label
         htmlFor="file-input"
         className="space-between flex h-full w-full flex-col items-center justify-center hover:cursor-pointer"
@@ -28,10 +48,10 @@ const Dropzone = () => {
           />
         </svg>
 
-        <p className="pt-2 text-sm">Select or drag/drop file here</p>
+        <p className="pt-2 text-sm">Drag & drop file here</p>
         <input className="hidden" type="file" id="file-input" />
         {file && (
-          <div className="mt-10 flex rounded-md border-2 border-solid border-black px-5 py-2">
+          <div className="mt-10 flex min-w-fit rounded-md border-2 border-solid border-black px-5 py-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -46,7 +66,26 @@ const Dropzone = () => {
                 d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12"
               />
             </svg>
-            <p className="mx-2">{file}</p>
+            <p className="mx-2">{file.name}</p>
+            <div
+              onClick={(e) => {
+                e.preventDefault();
+                setFile(null);
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
           </div>
         )}
       </label>
